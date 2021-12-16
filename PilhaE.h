44a25efefb1b9131a8a833eb3d43661e409ac1d3
claudioalberto07 <claudioalberto.jrf@gmail.com>
2021-12-16 19:33:01 -0200

    Mudança de pilha estática para aceitar diferentes tipos de dados para construção da pilha.

--- PilhaE.h
+++ PilhaE.h
@@ -5,19 +5,37 @@
 #ifndef ED_PILHAE_H
 #define ED_PILHAE_H
 
+template <class Type>
 
 class PilhaE {
+
 private:
-    int * vet;
+    Type * vet;
     int maxTam;
     int topo;
 public:
-    PilhaE();
-    ~PilhaE();
-    void push(int e);
-    void pop();
-    int getTopo();
-    bool vazia();
+    PilhaE(){
+        topo = -1;
+        vet = new int[100];
+        maxTam = 99;
+    }
+    ~PilhaE() {
+        delete []vet;
+    }
+
+    void push(Type e){
+        vet[++topo] = e;
+    }
+    void pop(){
+        topo--;
+    }
+    int getTopo(){
+        return vet[topo];
+    }
+
+    bool vazia(){
+
+    }
 
 };
 
